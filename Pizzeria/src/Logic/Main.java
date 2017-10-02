1cc7b8285ab305c1f80da65ac9bd571fda9a15a3
z0976190100 <z0976190100@gmail.com>
2017-10-02 23:21:58 +0300

    debugging filepath in Main

--- Pizzeria/src/Logic/Main.java
+++ Pizzeria/src/Logic/Main.java
@@ -10,14 +10,14 @@ public class Main {
 
         public static void main(String[] args) throws Exception {
             FileRead ff = new FileRead();
-
+String fileName = "C://Users/admin/OneDrive/codeacademy/IdeaProjects/a-level/PizzeriaGit/Pizzeria/src/Logic/Pizzeria.txt";
             List<String> menu
-                    = new ArrayList<>(ff.readByAnchors("Pizzeria.txt",
+                    = new ArrayList<>(ff.readByAnchors(fileName,
                     "-menu" , "--menu"));
             System.out.println(menu);
 
             List<String> order
-                    = new ArrayList<>(ff.readByAnchors("Pizzeria.txt",
+                    = new ArrayList<>(ff.readByAnchors(fileName,
                     "-orders", "--orders"));
             System.out.println(order);
 
