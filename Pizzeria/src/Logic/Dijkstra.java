b7564cec34a773014c9b8519817cd8a1340fa759
z0976190100 <z0976190100@gmail.com>
2017-10-03 15:37:08 +0300

    txt changes, dijkstra class added

---
+++ Pizzeria/src/Logic/Dijkstra.java
@@ -0,0 +1,10 @@
+package Logic;
+
+public class Dijkstra {
+
+    // map<String, Integer> 'neighbours' of matches between neighbour nodes and cost of match [node1;node2]=cost
+    //map<String, map<String, Integer> 'cityRouts' of routes [node1;node10]=[[node1;node n;node10]=cost]
+    //?set for comparing incoming route with existing ones;
+    //to calculate cost of incoming rout comparing pairs from 'cityRouts', if not exist, calculate by algorithm
+
+}
