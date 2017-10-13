a2f7a913026912102372851bff2447c0f28bc864
z0976190100 <z0976190100@gmail.com>
2017-10-13 21:16:40 +0300

    courier class added

---
+++ Pizzeria/src/Logic/Courier.java
@@ -0,0 +1,16 @@
+package Logic;
+
+public class Courier implements Runnable {
+
+     static int id;
+
+     int itsID;
+     String name;
+     boolean busy;
+
+     @Override
+    public void run() {
+
+     }
+
+}
