00628c146ca955cf19c24b7d03fbae0b4f1ee9b9
z0976190100 <z0976190100@gmail.com>
2017-10-16 22:40:54 +0300

    courier class added

--- Pizzeria/src/Logic/Courier.java
+++ Pizzeria/src/Logic/Courier.java
@@ -1,16 +1,38 @@
 package Logic;
 
-public class Courier implements Runnable {
+import java.util.HashMap;
+import java.util.Map;
 
-     static int id;
+import static Logic.Manager.orderDoneString;
+import static Logic.Manager.orderString;
 
-     int itsID;
-     String name;
-     boolean busy;
+public class Courier extends Thread {
 
-     @Override
+    static int id = 1;
+
+    int itsID;
+    int executOrder = 0;
+    String name;
+    boolean busy;
+    int travelingLength;
+    public static Map<Courier, Integer> couriersTable = new HashMap<>();
+
+    Courier(String nam) {
+        this.itsID = id++;
+        this.busy = false;
+        this.name = nam;
+        couriersTable.put(this, this.itsID);
+    }
+
     public void run() {
+        System.err.println("order no "+this.executOrder);
+        this.executingOrder(executOrder);
 
-     }
+    }
+// removes order id entrie from orderString map;
+    private void executingOrder(int ordId) {
+        System.err.println(orderDoneString.put(ordId, "done"));
+
+    }
 
 }
